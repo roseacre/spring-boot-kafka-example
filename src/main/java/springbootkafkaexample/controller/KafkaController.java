3162e243cd1050589109a9d3583419ab15453f2b
Fiona Evans <f.e.evans@btinternet.com>
2025-08-13 16:45:33 +0100

    Create KafkaController.java

---
+++ src/main/java/springbootkafkaexample/controller/KafkaController.java
@@ -0,0 +1,22 @@
+package springbootkafkaexample.controller;
+
+import springbootkafkaexample.service.KafkaProducerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+
+    private final KafkaProducerService kafkaProducerService;
+
+    public KafkaController(KafkaProducerService kafkaProducerService) {
+        this.kafkaProducerService = kafkaProducerService;
+    }
+
+    @GetMapping("/send")
+    public String sendMessage(@RequestParam String message) {
+        kafkaProducerService.sendMessage(message);
+        return "Message sent successfully";
+    }
+}
