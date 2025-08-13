5007e64ede7b29300748ac18af2313dded475424
Fiona Evans <f.e.evans@btinternet.com>
2025-08-13 16:51:25 +0100

    Create SpringBootKafkaExampleApplicationTests.java

---
+++ src/test/springbootkafkaexample/SpringBootKafkaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootkafkaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
