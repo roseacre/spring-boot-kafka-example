30aaec7a3edcb6620a577da1da074d02246412be
Fiona Evans <f.e.evans@btinternet.com>
2025-08-13 16:44:23 +0100

    Update KafkaConsumerConfig.java

--- src/main/java/springbootkafkaexample/config/KafkaConsumerConfig.java
+++ src/main/java/springbootkafkaexample/config/KafkaConsumerConfig.java
@@ -1,4 +1,4 @@
-package uk.gov.dwp.health.springbootkafkaexample.config;
+package springbootkafkaexample.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
